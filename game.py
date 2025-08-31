031e3ce11bf0f1ea2c6ba3333191bc91b9bebc3e
Anderrh <andercode@protonmail.com>
2025-08-31 13:39:58 -0700

    inventory done and world items taken

--- game.py
+++ game.py
@@ -1,7 +1,10 @@
+import os
+import sys
 import json
 import random
 
 def main():
+  print("\n"*50)
   with open("world_modern.json", "r") as world_file:
     world = json.loads(world_file.read())
   
@@ -20,27 +23,99 @@ def get_room_by_id(world, current_room):
 
 def find_exits(room):
   exits = ""
-  for (index, exit_name) in enumerate(room['exits']):
-    if index == 0:
-      exits += exit_name
-    else:
-      exits += "," + exit_name
+  
+  for exit_name in room['exits']:
+    
+    
+    exits += "\n "
+    exits += exit_name
+    for specific_exit in room['exits'][exit_name]:
+      short_desc = specific_exit["short_desc"]
+      exits += " - " + short_desc
   return exits
 
 
 
 def describe_room(world,current_room):
   room = get_room_by_id(world,current_room)
-  return (room['name'] + "\n-----------------\n" +room['description'] + " There are " + str(len (room['exits'])) + " exits: " + find_exits(room) + '.')
+
+  returns = (room['name'] + "\n-----------------\n" +room['description'])
+  if ((len (room['exits'])) == 1):
+    returns += " There is 1 destination:"
+  else:
+    returns += (" There are " + str(len (room['exits'])) + " destinations:")
+  
+  returns += find_exits(room)
+
+  return returns
+
+
+def go(world,inventory,room,direction):
+  if direction not in room["exits"]:
+    return room["id"] , "you can't go that way"
+  else:
+    for path in reversed(room["exits"][direction]):
+      # look at requires later
+      return path["to"], ("\n"*5) + path['traversal']
+  return room["id"] , "you can't go that way"
+  
 
 
 
 def game(world):
   current_room = random.choice(world["meta"]["spawn_points"])
   do_exit = False
+  inventory = []
   while (not do_exit):
     print(describe_room(world,current_room))
-    do_exit = True
+    text = input(">>> ")
+    print("\n"* 5)
+    if not text:
+      continue
+    command = text.split()[0].upper()
+    if command == "EXIT":
+      do_exit = True
+    if command == "RESTART":
+      os.execv(sys.executable, ['python'] + sys.argv)
+      return
+    if command == "GO":
+      current_room,text = go(world,inventory,get_room_by_id(world,current_room),text.split()[1].upper())
+      print(text + "\n")
+    if command == "GET":
+      room = get_room_by_id(world, current_room)
+      item_to_get = " ".join(text.split()[1:])
+      has_found = False
+      for i in reversed (range (len(room['items']))):
+        item = room['items'][i]
+        if (item_to_get.upper() == item['name'].upper() ):
+          inventory.append(item)
+          room['items'] = room['items'][:i]+room['items'][i+1:]
+          has_found = True
+          print(f"You take the {item['name']}\n")
+      if not has_found:
+        print (f"You search and you cannot find the {item_to_get}\n") 
+    
+
+      
+        
+      
+    if command == 'I':
+
+      inventory_desc = ""
+
+      for inventory_item in sorted(inventory,key=lambda item: item["name"]):
+        
+        inventory_desc += inventory_item["name"]
+        inventory_desc += (" - ")
+        inventory_desc += inventory_item['check']["notes"]
+        inventory_desc += ("\n")
+
+
+        
+      print (inventory_desc)
+      print('')
+
+
 
 
 
